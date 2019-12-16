58cdc8248c0e4e7c5bd03962e62b31fcf0f58dfb
Romullosilva <romullo.dsilva@gmail.com>
2019-12-16 17:28:45 -0300

    Blog Cine com rotas no Angular

--- first/src/app/app-routing.module.ts
+++ first/src/app/app-routing.module.ts
@@ -2,7 +2,24 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+
+/*Aqui fazemos a importação dos componentes do site */
+import { MenuComponent } from './menu/menu.component';
+import { VideosComponent } from './videos/videos.component';
+import {CorpoComponent} from './corpo/corpo.component';
+import { FormularioComponent} from './formulario/formulario.component';
+
+
+
+const routes: Routes = [
+
+{path: "", redirectTo: 'corpo', pathMatch: 'full'},
+{path: 'corpo', component: CorpoComponent },
+{path: 'menu', component: MenuComponent},
+{path: 'videos', component: VideosComponent },
+{path: 'formulario', component: FormularioComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
